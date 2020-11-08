baca91df281b756a9ccd1f702dffa9f0ef961548
marketveshey <fitisovn@gmail.com>
2020-11-09 00:36:57 +0200

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: false,
+        arrows: true,   
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+  });
